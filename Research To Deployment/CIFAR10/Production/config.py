9021012a030adb8ba56b83bc7f1438794b515790
sgudiduri <siddu.gemeni@gmail.com>
2023-01-02 18:04:16 -0600

    update

---
+++ Research To Deployment/CIFAR10/Production/config.py
@@ -0,0 +1,11 @@
+# MODEL FITTING
+IMAGE_SIZE = 32 # 32 for testing, 150 for final model
+
+#Hyperparameters
+BATCH_SIZE = 128
+LEARNING_RATE = 0.001
+WEIGHT_DECAY = 0.0001
+EPOCHS = 10  # 1 for testing, 10 for final model
+
+# MODEL PERSISTING
+MODEL_PATH = "cnn_model.h5"
