8a1380540f224b7a0d44923e0a1206635ee1fdda
sgudiduri <siddu.gemeni@gmail.com>
2023-01-03 15:34:49 -0600

    Update config.py

--- ResearchToDeployment/CIFAR10/Production/config.py
+++ ResearchToDeployment/CIFAR10/Production/config.py
@@ -3,8 +3,7 @@ IMAGE_SIZE = 32 # 32 for testing, 150 for final model
 
 #Hyperparameters
 BATCH_SIZE = 128
-LEARNING_RATE = 0.001
-WEIGHT_DECAY = 0.0001
+OPTIMIZER = "adam"
 EPOCHS = 10  # 1 for testing, 10 for final model
 
 # MODEL PERSISTING
